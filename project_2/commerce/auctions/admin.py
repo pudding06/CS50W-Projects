83b47070cee0cd8f714423dedae3da8b73a16958
pudding06 <RKOH024@ntu.edu.sg>
2024-07-12 23:51:36 +0800

    updated admin.py file

--- project_2/commerce/auctions/admin.py
+++ project_2/commerce/auctions/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Category, Listings
+from .models import User, Category, Listings, Comment
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Listings)
+admin.site.register(Comment)
