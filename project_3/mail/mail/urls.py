059680f9b9ecb4dbdb3a6277c3c206ee4d1760a1
pudding06 <RKOH024@ntu.edu.sg>
2024-07-18 09:42:53 +0800

    send data from Compose to views.py via send route

--- project_3/mail/mail/urls.py
+++ project_3/mail/mail/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
+    path("send", views.send, name="send"),
 
     # API Routes
     path("emails", views.compose, name="compose"),
